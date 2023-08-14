8c56da33121ad127d8c7cb116188ae48d61f52ec
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-08-14 18:25:47 +0200

    Allow to change server info and theme in dev sandbox

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,11 +1,43 @@
-import type { FC } from 'react';
+import type { FC, FormEvent } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { Button, Input } from 'reactstrap';
 import { ShlinkWebComponent } from '../src';
 import { ShlinkApiClient } from './api/ShlinkApiClient';
+import { ThemeToggle } from './ThemeToggle';
 
-export const App: FC = () => (
-  <ShlinkWebComponent
-    serverVersion="3.6.2"
-    apiClient={new ShlinkApiClient('https://acel.me', '')}
-    settings={{}}
-  />
-);
+export const App: FC = () => {
+  const [serverInfo, setServerInfo] = useState<{ baseUrl?: string; apiKey?: string }>({});
+  const handleSubmit = useCallback((e: FormEvent) => {
+    e.preventDefault();
+    // @ts-expect-error - Entries is not recognized for some reason
+    setServerInfo(Object.fromEntries(new FormData(e.target).entries()));
+  }, []);
+  const apiClient = useMemo(
+    () => serverInfo.apiKey && serverInfo.baseUrl && new ShlinkApiClient(serverInfo.baseUrl, serverInfo.apiKey),
+    [serverInfo],
+  );
+
+  return (
+    <>
+      <header className="header fixed-top text-white d-flex justify-content-between">
+        <form className="py-2 ps-2 d-flex gap-2" onSubmit={handleSubmit}>
+          <Input name="baseUrl" placeholder="Server URL" type="url" />
+          <Input name="apiKey" placeholder="API key" />
+          <Button type="submit" color="light">Save</Button>
+        </form>
+        <div className="h-100 text-end pe-3 pt-3">
+          <ThemeToggle />
+        </div>
+      </header>
+      <div className="wrapper">
+        {apiClient && (
+          <ShlinkWebComponent
+            serverVersion="3.6.2"
+            apiClient={apiClient}
+            settings={{}}
+          />
+        )}
+      </div>
+    </>
+  );
+};

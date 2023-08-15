ad6a7b6e379a64c144d119fe826fa15bdab01956
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-08-15 09:47:12 +0200

    Update to @testing-library/jest-dom:6.0.0

--- test/setup.ts
+++ test/setup.ts
@@ -1,23 +1,13 @@
 import 'vitest-canvas-mock';
 import 'chart.js/auto';
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-import matchers from '@testing-library/jest-dom/matchers';
+import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 import ResizeObserver from 'resize-observer-polyfill';
-import { afterEach, expect } from 'vitest';
+import { afterEach } from 'vitest';
 
-// Workaround for TypeScript error: https://github.com/testing-library/jest-dom/issues/439#issuecomment-1536524120
-declare module 'vitest' {
-  interface Assertion<T = any> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> {}
-}
-
-// Extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers);
-
+// Clears all mocks and cleanup DOM after every test
 afterEach(() => {
-  // Clears all mocks after every test
   vi.clearAllMocks();
-  // Run a cleanup after each test case (e.g. clearing jsdom)
   cleanup();
 });
 

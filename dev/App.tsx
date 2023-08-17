19391b567347de6ae37c105dc8f8164fa13bb653
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-08-17 09:06:49 +0200

    Add persistent config in dev sandbox

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,16 +1,17 @@
 import type { FC, FormEvent } from 'react';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo } from 'react';
 import { Button, Input } from 'reactstrap';
 import { ShlinkWebComponent } from '../src';
 import { ShlinkApiClient } from './api/ShlinkApiClient';
+import { useServerInfo } from './hooks/useServerInfo';
 import { ThemeToggle } from './ThemeToggle';
 
 export const App: FC = () => {
-  const [serverInfo, setServerInfo] = useState<{ baseUrl?: string; apiKey?: string }>({});
+  const [serverInfo, updateServerInfo] = useServerInfo();
   const handleSubmit = useCallback((e: FormEvent) => {
     e.preventDefault();
     // @ts-expect-error - Entries is not recognized for some reason
-    setServerInfo(Object.fromEntries(new FormData(e.target).entries()));
+    updateServerInfo(Object.fromEntries(new FormData(e.target).entries()));
   }, []);
   const apiClient = useMemo(
     () => serverInfo.apiKey && serverInfo.baseUrl && new ShlinkApiClient(serverInfo.baseUrl, serverInfo.apiKey),
@@ -21,9 +22,9 @@ export const App: FC = () => {
     <>
       <header className="header fixed-top text-white d-flex justify-content-between">
         <form className="py-2 ps-2 d-flex gap-2" onSubmit={handleSubmit}>
-          <Input name="baseUrl" placeholder="Server URL" type="url" />
-          <Input name="apiKey" placeholder="API key" />
-          <Button type="submit" color="light">Save</Button>
+          <Input name="baseUrl" placeholder="Server URL" type="url" defaultValue={serverInfo.baseUrl} />
+          <Input name="apiKey" placeholder="API key" defaultValue={serverInfo.apiKey} />
+          <Button type="submit" color="light">Load</Button>
         </form>
         <div className="h-100 text-end pe-3 pt-3">
           <ThemeToggle />
--- dev/ThemeToggle.tsx
+++ dev/ThemeToggle.tsx
@@ -1,12 +1,16 @@
 import { changeThemeInMarkup, ToggleSwitch } from '@shlinkio/shlink-frontend-kit';
 import type { FC } from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export const ThemeToggle: FC = () => {
-  const [checked, setChecked] = useState(false);
+  const query = useMemo(() => new URLSearchParams(location.search), []);
+  const [checked, setChecked] = useState(query.get('theme') === 'dark');
 
   useEffect(() => {
-    changeThemeInMarkup(checked ? 'dark' : 'light');
+    const theme = checked ? 'dark' : 'light';
+    changeThemeInMarkup(theme);
+    query.set('theme', theme);
+    history.pushState({}, '', `?${query}`);
   }, [checked]);
 
   return <ToggleSwitch checked={checked} onChange={setChecked}>Dark theme</ToggleSwitch>;

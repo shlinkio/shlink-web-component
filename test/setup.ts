0e043d90072785b82b313728d2b8293e3cc19eba
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-09-24 19:23:15 +0200

    Add axe-core config

--- test/setup.ts
+++ test/setup.ts
@@ -2,8 +2,19 @@ import 'vitest-canvas-mock';
 import 'chart.js/auto';
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
+import axe from 'axe-core';
 import ResizeObserver from 'resize-observer-polyfill';
 import { afterEach } from 'vitest';
+
+axe.configure({
+  checks: [
+    {
+      // Disable color contrast checking, as it doesn't work in jsdom
+      id: 'color-contrast',
+      enabled: false,
+    },
+  ],
+});
 
 // Clears all mocks and cleanup DOM after every test
 afterEach(() => {

d9edd7e2c39f60bcfba7b96271762d9350871aa8
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-04-08 11:11:13 +0200

    Run tests in a headless browser with vitest browser mode and playwright

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom/vitest';
 import axe from 'axe-core';
-import ResizeObserver from 'resize-observer-polyfill';
 import { beforeEach } from 'vitest';
 
 axe.configure({
@@ -17,13 +16,3 @@ axe.configure({
 beforeEach(() => {
   vi.clearAllMocks();
 });
-
-HTMLCanvasElement.prototype.getContext = (() => {}) as any;
-
-// Stuff JSDOM does not implement
-(global as any).URL.createObjectURL = () => '';
-(global as any).ResizeObserver = ResizeObserver;
-(global as any).scrollTo = () => {};
-(global as any).prompt = () => {};
-(global as any).matchMedia = (media: string) => ({ matches: false, media });
-(global as any).HTMLElement.prototype.scrollIntoView = () => {};

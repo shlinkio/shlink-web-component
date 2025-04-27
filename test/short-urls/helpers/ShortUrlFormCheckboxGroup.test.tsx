46092e87e04ec5323a183980cf44444fbccc06b0
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-04-27 13:00:23 +0200

    Migrate ShortUrlForm to tailwind

--- test/short-urls/helpers/ShortUrlFormCheckboxGroup.test.tsx
+++ test/short-urls/helpers/ShortUrlFormCheckboxGroup.test.tsx
@@ -1,30 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import type {
-  ShortUrlFormCheckboxGroupProps,
-} from '../../../src/short-urls/helpers/ShortUrlFormCheckboxGroup';
-import {
-  ShortUrlFormCheckboxGroup,
-} from '../../../src/short-urls/helpers/ShortUrlFormCheckboxGroup';
+import { render } from '@testing-library/react';
+import type { ShortUrlFormCheckboxGroupProps } from '../../../src/short-urls/helpers/ShortUrlFormCheckboxGroup';
+import { ShortUrlFormCheckboxGroup } from '../../../src/short-urls/helpers/ShortUrlFormCheckboxGroup';
 import { checkAccessibility } from '../../__helpers__/accessibility';
 
 describe('<ShortUrlFormCheckboxGroup />', () => {
-  const setUp = (props: ShortUrlFormCheckboxGroupProps = {}) => render(<ShortUrlFormCheckboxGroup {...props} />);
+  const setUp = (props: Omit<ShortUrlFormCheckboxGroupProps, 'infoTooltip'> = {}) => render(
+    <ShortUrlFormCheckboxGroup infoTooltip="This is the tooltip" {...props} />,
+  );
 
-  it.each([
-    [undefined],
-    ['This is the tooltip'],
-  ])('passes a11y checks', (infoTooltip) => checkAccessibility(setUp({ infoTooltip, children: 'A checkbox' })));
-
-  it.each([
-    [undefined, '', 0],
-    ['This is the tooltip', 'me-2', 1],
-  ])('renders tooltip only when provided', (infoTooltip, expectedClassName, expectedAmountOfTooltips) => {
-    setUp({ infoTooltip });
-
-    expect(screen.getByRole('checkbox').parentNode).toHaveAttribute(
-      'class',
-      expect.stringContaining(expectedClassName),
-    );
-    expect(screen.queryAllByRole('img', { hidden: true })).toHaveLength(expectedAmountOfTooltips);
-  });
+  it('passes a11y checks', () => checkAccessibility(setUp({ children: 'A checkbox' })));
 });

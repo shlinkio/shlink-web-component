caa7c8e29745a1e37de6c5a4cfc2b084a3b39234
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-05-30 09:17:04 +0200

    Migrate ColorInput to tailwind

--- src/utils/components/ColorInput.tsx
+++ src/utils/components/ColorInput.tsx
@@ -1,19 +1,19 @@
-import { useElementRef } from '@shlinkio/shlink-frontend-kit';
+import { Input } from '@shlinkio/shlink-frontend-kit/tailwind';
 import type { FC } from 'react';
-import { Input, InputGroup } from 'reactstrap';
+import { useRef } from 'react';
 import type { ColorPickerProps } from './ColorPicker';
 import { ColorPicker } from './ColorPicker';
 
 export const ColorInput: FC<Omit<ColorPickerProps, 'className'>> = ({ color, onChange, name }) => {
-  const colorPickerRef = useElementRef<HTMLInputElement>();
+  const colorPickerRef = useRef<HTMLInputElement>(null);
 
   return (
-    <InputGroup>
+    <div className="tw:flex">
       <ColorPicker
         name={`${name}-picker`}
         color={color}
         onChange={onChange}
-        className="input-group-text"
+        className="tw:rounded-r-none"
         ref={colorPickerRef}
       />
       <Input
@@ -22,7 +22,8 @@ export const ColorInput: FC<Omit<ColorPickerProps, 'className'>> = ({ color, onC
         onClick={() => colorPickerRef.current?.click()}
         aria-label={name}
         data-testid="text-input"
+        className="tw:grow tw:rounded-l-none"
       />
-    </InputGroup>
+    </div>
   );
 };

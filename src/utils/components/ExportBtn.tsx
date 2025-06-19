b32897f2ca07ffd8a69c26e4d9358164bfe3d686
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-06-19 08:19:13 +0200

    Fix spaces in import

--- src/utils/components/ExportBtn.tsx
+++ src/utils/components/ExportBtn.tsx
@@ -1,7 +1,7 @@
 import { faFileCsv } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import type { ButtonProps } from '@shlinkio/shlink-frontend-kit';
-import { Button, formatNumber,isDarkThemeEnabled   } from '@shlinkio/shlink-frontend-kit';
+import { Button, formatNumber, isDarkThemeEnabled } from '@shlinkio/shlink-frontend-kit';
 import type { FC } from 'react';
 
 export type ExportBtnProps = Omit<ButtonProps, 'disabled' | 'to'> & {

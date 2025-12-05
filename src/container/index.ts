0db6171eee81f08d3b861bb377edb16387a2559a
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-12-05 11:15:16 +0100

    Move the few remaining service registration to container index module

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,10 +1,24 @@
+import { useTimeoutToggle } from '@shlinkio/shlink-frontend-kit';
 import Bottle from 'bottlejs';
-import { provideServices as provideUtilsServices } from '../utils/services/provideServices';
-import { provideServices as provideVisitsServices } from '../visits/services/provideServices';
+import { jsonToCsv } from '../utils/helpers/json';
+import { ColorGenerator } from '../utils/services/ColorGenerator';
+import { ImageDownloader } from '../utils/services/ImageDownloader';
+import { ReportExporter } from '../utils/services/ReportExporter';
+import * as visitsParser from '../visits/services/VisitsParser';
 
 export const bottle = new Bottle();
 
 export const { container } = bottle;
 
-provideVisitsServices(bottle);
-provideUtilsServices(bottle);
+bottle.constant('window', window);
+bottle.constant('fetch', window.fetch.bind(window));
+bottle.service('ImageDownloader', ImageDownloader, 'fetch', 'window');
+
+bottle.service('ColorGenerator', ColorGenerator, 'TagColorsStorage');
+
+bottle.constant('jsonToCsv', jsonToCsv);
+bottle.service('ReportExporter', ReportExporter, 'window', 'jsonToCsv');
+
+bottle.serviceFactory('useTimeoutToggle', () => useTimeoutToggle);
+
+bottle.serviceFactory('VisitsParser', () => visitsParser);
--- src/visits/services/provideServices.ts
+++
@@ -1,6 +0,0 @@
-import type Bottle from 'bottlejs';
-import * as visitsParser from './VisitsParser';
-
-export const provideServices = (bottle: Bottle) => {
-  bottle.serviceFactory('VisitsParser', () => visitsParser);
-};

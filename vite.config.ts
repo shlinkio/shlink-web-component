9dd02c06e23f2c689bc2bc9ad01f4060770dd0f8
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-08-19 10:18:22 +0200

    Fix project building

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { resolve } from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { playwright } from '@vitest/browser-playwright';
 import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vitest/config';
 import pack from './package.json' with { type: 'json' };
-
-const { dirname } = import.meta;
 
 export default defineConfig({
   plugins: [
@@ -21,9 +18,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: {
-        index: resolve(dirname, 'src/index.ts'),
-        'api-contract': resolve(dirname, 'src/api-contract/index.ts'),
-        settings: resolve(dirname, 'src/settings/index.ts'),
+        index: './src/index.ts',
+        'api-contract': './src/api-contract/index.ts',
+        settings: './src/settings/index.ts',
       },
       name: 'shlink-web-component',
       formats: ['es'], // Generate ES module only
